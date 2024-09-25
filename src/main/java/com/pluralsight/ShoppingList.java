c2943909d983a5f03667dec2069eef720d2a1ff3
Osmig_Torres <torresosmig@gmail.com>
2024-09-25 13:29:07 -0700

    added files

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        String[] list = {"hey", "not"};
+        for (String item: list) {
+            System.out.println(item);
+        }
+    }
+}
