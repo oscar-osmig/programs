cacc6b284df112f0a4b60bbebbb64315f313b497
Osmig_Torres <torresosmig@gmail.com>
2024-09-26 09:03:06 -0700

    modified ShppingList class

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -2,7 +2,7 @@ package com.pluralsight;
 
 public class ShoppingList {
     public static void main(String[] args) {
-        String[] list = {"hey", "not"};
+        String[] list = {"chicken", "meat", "rice", "corn", "choco", "banana", "hot dog", "t shirt", "juice", "apple"};
         for (String item: list) {
             System.out.println(item);
         }
